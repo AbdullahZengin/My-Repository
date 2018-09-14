0563bb74ccd1e476e63b7a84a7e7459e27e3c916
AbdullahZengin <abdullah-zengin@hotmail.com.tr>
2018-09-14 19:01:58 +0300

    asd

---
+++ UtfToAscii/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Abdullah ZENGİN";
+
+            Console.WriteLine(Utf8ToAscii(input));
+
+            Console.ReadKey();
+        }
+
+        static string Utf8ToAscii(string input)
+        {
+            string asciiCode = "";
+            var utf8bytes = Encoding.UTF8.GetBytes(input);
+            var win1252Bytes = Encoding.Convert(Encoding.UTF8, Encoding.ASCII, utf8bytes);
+            foreach (var item in win1252Bytes)
+            {
+                asciiCode += item + " ";
+            }
+
+            return asciiCode;
+        }
+    }
+}
